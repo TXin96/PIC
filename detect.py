3a51842f8177270da3830eb6e64bd0d00bdebf3f
TXin96 <609012488@qq.com>
2018-07-21 11:55:20 +0800

    Generate semantic and instance segmenation image (gray scale), json file as well.

--- detect.py
+++ detect.py
@@ -1,21 +1,16 @@
 import os
-import random
-import numpy as np
-import matplotlib.pyplot as plt
 import json
-
-import utils
+import numpy as np
+from PIL import Image
+import cv2
 import model as modellib
-import visualize
 from picConfig import PicConfig
-from picConfig import PicDataset
 
 # Detection
 ROOT_DIR = os.getcwd()
 
 # Directory to save logs and trained model
 MODEL_DIR = os.path.join(ROOT_DIR, "logs")
-
 MODEL_PATH = os.path.join(MODEL_DIR, "pic.h5")
 
 # Directory of the images
@@ -25,30 +20,10 @@ TRAIN_SEGMENTATION_PATH = "segmentation/train"
 VAL_IMAGE_PATH = "image/val"
 VAL_SEGMENTATION_PATH = "segmentation/val"
 
-config = PicConfig()
-config.display()
+TEST_IMAGE_PATH = "test/image"
 
-# Training dataset
-dataset_train = PicDataset()
-dataset_train.load_pic(TRAIN_IMAGE_PATH, TRAIN_SEGMENTATION_PATH)
-dataset_train.prepare()
-
-# Validation dataset
-dataset_val = PicDataset()
-dataset_val.load_pic(VAL_IMAGE_PATH, VAL_SEGMENTATION_PATH)
-dataset_val.prepare()
-
-
-def get_ax(rows=1, cols=1, size=8):
-    """Return a Matplotlib Axes array to be used in
-    all visualizations in the notebook. Provide a
-    central point to control graph sizes.
-
-    Change the default size attribute to control the size
-    of rendered images
-    """
-    _, ax = plt.subplots(rows, cols, figsize=(size * cols, size * rows))
-    return ax
+INSTANCE_SAVE_PATH = os.path.join(ROOT_DIR, "output/instance")
+SEMANTIC_SAVE_PATH = os.path.join(ROOT_DIR, "output/semantic")
 
 
 class InferenceConfig(PicConfig):
@@ -56,47 +31,59 @@ class InferenceConfig(PicConfig):
     IMAGES_PER_GPU = 1
 
 
-inference_config = InferenceConfig()
+def detect(image_dir, instance_save_path, semantic_save_path):
+    if not os.path.exists(instance_save_path):
+        os.makedirs(instance_save_path)
 
-# Recreate the model in inference mode
-model = modellib.MaskRCNN(mode="inference",
-                          config=inference_config,
-                          model_dir=MODEL_DIR)
+    if not os.path.exists(semantic_save_path):
+        os.makedirs(semantic_save_path)
+    inference_config = InferenceConfig()
 
-# Get path to saved weights
-# Either set a specific path or find last trained weights
-# model_path = os.path.join(ROOT_DIR, ".h5 file name here")
+    # Recreate the model in inference mode
+    model = modellib.MaskRCNN(mode="inference",
+                              config=inference_config,
+                              model_dir=MODEL_DIR)
 
-# Load trained weights
-print("Loading weights from ", MODEL_PATH)
-model.load_weights(MODEL_PATH, by_name=True)
+    # Load trained weights
+    print("Loading weights from ", MODEL_PATH)
+    model.load_weights(MODEL_PATH, by_name=True)
 
-# Test on a random image
-image_id = random.choice(dataset_val.image_ids)
-image, image_meta, gt_class_id, gt_bbox, gt_mask = \
-    modellib.load_image_gt(dataset_val, config, image_id, use_mini_mask=False)
-info = dataset_val.image_info[image_id]
-print("image ID: {}.{} ({}) {}".format(info["source"], info["id"], image_id,
-                                       dataset_val.image_reference(image_id)))
-# Run object detection
-results = model.detect([image], verbose=1)
+    res = []
+    with open('result.json', 'w') as outfile:
+        datafiles = os.listdir(image_dir)
 
-# Display results
-ax = get_ax(1)
-r = results[0]
+        for file in datafiles:
+            image = cv2.imread(os.path.join(image_dir, file))
+            results = model.detect([image], verbose=1)
+            r = results[0]
 
-visualize.display_instances(image, boxes=r['rois'], masks=r['masks'], class_ids=r['class_ids'],
-                            class_names=dataset_val.class_names, scores=r['scores'], ax=ax,
-                            figsize=[image.shape[0], image.shape[1]],
-                            title="Predictions")
+            masks = r['masks']
+            class_id = r['class_ids']
+            instance_num = r['rois'].shape[0]
 
-for i in r:
-    r[i] = r[i].tolist()
-r.pop('masks')
-r.pop('scores')
-r['image_name'] = info['id'] + '.jpg'
-with open('result.json', 'w') as outfile:
-    json.dump(r, outfile, ensure_ascii=False)
+            instance_image = np.zeros((masks.shape[0], masks.shape[1])).astype(np.uint8)
+            semantic_image = np.zeros((masks.shape[0], masks.shape[1])).astype(np.uint8)
+
+            for j in range(instance_num):
+                print(j)
+                mask = masks[:, :, j]
+                print(mask.shape)
+                instance_image[:, :] = np.where(mask == 1, j + 1, instance_image[:, :])
+                semantic_image[:, :] = np.where(mask == 1, class_id[j], semantic_image[:, :])
+
+            instance_image = Image.fromarray(instance_image)
+            instance_image.save(os.path.join(instance_save_path, file.replace('.jpg', '.png')))
+            semantic_image = Image.fromarray(semantic_image)
+            semantic_image.save(os.path.join(semantic_save_path, file.replace('.jpg', '.png')))
+
+            for k in r:
+                r[k] = r[k].tolist()
+            r.pop('masks')
+            r.pop('scores')
+            r['image_name'] = file
+            res.append(r)
+        json.dump(res, outfile, ensure_ascii=False)
 
 
-# Evaluation
+if __name__ == '__main__':
+    detect(VAL_IMAGE_PATH, INSTANCE_SAVE_PATH, SEMANTIC_SAVE_PATH)
